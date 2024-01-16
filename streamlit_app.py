609952a06096db82d9f13bb377637f6cd18b2c82
Parsa Mofrad <parsamofrad@Parsas-MacBook-Pro.local>
2024-01-16 10:35:51 +1030

    adding first changes and setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import streamlit as st
 
+# Load the dataset
+df = pd.read_csv('spotify_songs.csv')  # make sure the file is in the same directory as your script
+
+# Extract the specific columns
+columns_to_extract = ['track_name', 'track_artist', 'track_popularity', 'playlist_genre']
+extracted_df = df[columns_to_extract]
+
+# Display the DataFrame in the Streamlit app
+st.write(extracted_df)
