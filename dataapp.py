2c077aa21b40cededd4108f660c69fc65330bee0
Parsa Mofrad <parsamofrad@Parsas-MacBook-Pro.local>
2024-01-16 13:49:59 +1030

    new feature

--- dataapp.py
+++ dataapp.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import streamlit as st
+import gender_guesser.detector as gr
 import plotly.express as px
 from textblob import TextBlob
+
+st.markdown('# Welcome to the 2020 Spotify music dashboard.')
+data_url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv"
+
+# create a button to take user to original data
+st.markdown(f'<a href="{data_url}" target="_blank"><button style="color: white; background-color: #FF4B4B; border: None; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Go to Original Data</button></a>', unsafe_allow_html=True)
 
 # loading dataset and extracting columns of interest
 df = pd.read_csv('spotify_songs.csv')  # make sure the file is in the same directory as your script
@@ -36,3 +43,21 @@ sentimentCount = extracted_df_copy["sentiment"].value_counts()
 fig_sentiment = px.pie(sentimentCount, values=sentimentCount, names=sentimentCount.index, title='Distribution of Sentiments in Song Titles')
 st.write("Here is a pie chart of the distribution of sentiments in song titles:")
 st.plotly_chart(fig_sentiment)
+
+# detecting gender of artist based on their name
+detector = gr.Detector()
+
+def genderAnalysis(name):
+    if isinstance(name, str):
+        firstName = name.split()[0]
+        return detector.get_gender(firstName)
+    else:
+        return 'unknown'
+
+
+extracted_df_copy["gender"] = extracted_df_copy["track_artist"].apply(genderAnalysis)
+gendCount = extracted_df_copy["gender"].value_counts()
+
+figGender = px.pie(gendCount, values = gendCount, names = gendCount.index, title='Distribution of genders across artists')
+st.write("Here's the distribution of genders from artist names")
+st.plotly_chart(figGender)
