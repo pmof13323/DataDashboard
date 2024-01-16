09b2af3cbc74744fb407baaa3bcf8c230f52c710
Parsa Mofrad <parsamofrad@Parsas-MacBook-Pro.local>
2024-01-16 12:09:17 +1030

    using plotly instead

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import plotly.express as px
 from textblob import TextBlob
 
 # loading dataset and extracting columns of interest
@@ -9,17 +9,12 @@ df = pd.read_csv('spotify_songs.csv')  # make sure the file is in the same direc
 columns_to_extract = ['track_name', 'track_artist', 'track_popularity', 'playlist_genre']
 extracted_df = df[columns_to_extract]
 
-# creating pie chart for genre distribution
+# creating pie chart for genre distribution using plotly
 genre_counts = extracted_df['playlist_genre'].value_counts()
 
+fig_genre = px.pie(genre_counts, values=genre_counts, names=genre_counts.index, title='Distribution of Playlist Genres')
 st.write("Here is a pie chart of the distribution of genres in the 2020 data:")
-
-plt.figure(figsize=(10, 6))
-plt.pie(genre_counts, labels=genre_counts.index, autopct='%1.1f%%', startangle=140)
-plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
-plt.title('Distribution of Playlist Genres')
-
-st.pyplot(plt)
+st.plotly_chart(fig_genre)
 
 # sentiment analysis of whether song titles were positive or negative on average
 def analysingSentiment(title):
@@ -37,10 +32,7 @@ extracted_df_copy = extracted_df.copy()
 extracted_df_copy["sentiment"] = extracted_df_copy["track_name"].apply(analysingSentiment)
 sentimentCount = extracted_df_copy["sentiment"].value_counts()
 
+# creating pie chart for sentiment analysis using plotly
+fig_sentiment = px.pie(sentimentCount, values=sentimentCount, names=sentimentCount.index, title='Distribution of Sentiments in Song Titles')
 st.write("Here is a pie chart of the distribution of sentiments in song titles:")
-
-plt.figure(figsize=(10, 6))
-plt.pie(sentimentCount, labels=sentimentCount.index, autopct='%1.1f%%', startangle=140)
-plt.axis('equal') 
-plt.title('Distribution of Sentiments in Song Titles')
-st.pyplot(plt)
+st.plotly_chart(fig_sentiment)
